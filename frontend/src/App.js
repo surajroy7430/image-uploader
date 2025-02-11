58afb45b9249c450ca6d272015bd2c359d7637f3
surajroy7430 <surajroy7430@gmail.com>
2025-02-11 12:55:12 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://minxsmusic-images.vercel.app";
 
 function App() {
   const [files, setFiles] = useState([]);
